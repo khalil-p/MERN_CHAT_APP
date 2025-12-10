a95d98f3eabea763b67d7675311175a1e821327f
Khalil Pathan <77223832+khalil-p@users.noreply.github.com>
2025-12-10 19:10:54 +0530

    Add files via upload

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.middleware.js"
 import { User } from "../models/user.model.js";
 import { generateJWTToken } from "../utils/jwtToken.js";
 import bcrypt from "bcryptjs";
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 export const signup = catchAsyncErrors(async (req, res, next) => {
   const { fullName, email, password } = req.body;
   if (!fullName || !email || !password) {
@@ -97,13 +97,13 @@ export const signout = catchAsyncErrors(async (req, res, next) => {
     });
 });
 export const getUser = catchAsyncErrors(async (req, res, next) => {
-  // const user = await User.findById(req.user._id)
   const user = req.user;
   res.status(200).json({
     success: true,
     user,
   });
 });
+
 export const updateProfile = catchAsyncErrors(async (req, res, next) => {
   const { email, fullName } = req.body;
   if (email.trim().length === 0 || fullName.trim().length === 0) {
@@ -112,19 +112,56 @@ export const updateProfile = catchAsyncErrors(async (req, res, next) => {
       message: "fullName and  email cannot be empty.",
     });
   }
-  const avatar  = req?.files?.avatar;
-  const cloudinaryResponse = {}
-  if(avatar){
-    try{
-const oldAvatarPublicId = req.user?.avatar?.public_id
-if(oldAvatarPublicId && oldAvatarPublicId.length > 0){
-  await cloudinary 
-}
-    }catch(error){
-
+  const avatar = req?.files?.avatar;
+  const cloudinaryResponse = {};
+  if (avatar) {
+    try {
+      const oldAvatarPublicId = req.user?.avatar?.public_id;
+      if (oldAvatarPublicId && oldAvatarPublicId.length > 0) {
+        await cloudinary.uploader.destroy(oldAvatarPublicId);
+      }
+      cloudinaryResponse = await cloudinary.uploader.upload(
+        avatar.tempFilePath,
+        {
+          folder: "CHAT_APP_USERS_AVATARS",
+          transformation: [
+            { width: 150, height: 150, crop: "limit" },
+            { quality: "auto" },
+            { fetch_format: "auto" },
+          ],
+        }
+      );
+    } catch (error) {
+      console.error("Cloudinary Upload Error:", error);
+      return res.status(500).json({
+        success: false,
+        message: "Failed to upload avatar image. Please try again later.",
+      });
     }
   }
-  const user = await User.findById(req.user._id);
-  if (email) {
+  let data = {
+    fullName,
+    email,
+  };
+  if (
+    avatar &&
+    cloudinaryResponse?.public_id &&
+    cloudinaryResponse?.secure_url
+  ) {
+    data.avatar = {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    };
   }
+
+  let user = await User.findByIdAndUpdate(req.user._id, data, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Profile updated successfully",
+    user,
+  });
 });
